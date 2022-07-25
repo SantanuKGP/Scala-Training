bca66b189843168c826bec8df8ee1afe97be6581
Santanu Kundu <s.kundu@adform.com>
2022-07-25 16:08:48 +0530

    SaaS problms

--- SAAS_Assignments/src/problem11.scala
+++ SAAS_Assignments/src/problem11.scala
@@ -1,3 +1,20 @@
-object problem11 {
-
+import scala.io.StdIn.{readInt, readLine}
+import scala.collection.mutable.{Map => mMap}
+object problem11 extends App{
+  var x=readInt()
+  var m= mMap[Int,Int]().withDefaultValue(0)
+  Range(0,x).foreach{ _ =>
+    val y=readLine().split( " ").map(x=> x.toInt)
+    m(y(0))=y(1)
+  }
+  x=readInt()
+  var (a,b)=(0,0)
+  val y=readLine().split(" ").map(_.toInt)
+  for(i <- 0 until x by 2){
+    a=a+y(i)
+    b=b+y(i+1)
+    while(m.contains(a)) a=m(a)
+    while(m.contains(b)) b=m(b)
+  }
+  if(a>b) println("A "+a) else println("B "+b)
 }
