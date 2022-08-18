2e2f1ee541580acf34d8208891d507cc3a236593
Santanu Kundu <s.kundu@adform.com>
2022-08-18 15:17:07 +0530

    SAAS Update

--- SAAS_Assignments/src/problem2.scala
+++ SAAS_Assignments/src/problem2.scala
@@ -3,15 +3,19 @@ import scala.io.Source._
 object problem2 extends App{
   def numberWord(file: String):Unit={
     var acc=0
-    val regex="[a-z_A-Z_0-9_-]+".r
+    val regex="[a-z_A-Z0-9-]+".r
     for(line <- fromFile(file).getLines()) acc+=regex.findAllMatchIn(line).toList.length
     println(s"Number of Words of $file is "+acc)
   }
 
   def numberWords(file: String):Unit={
-    val regex="[a-z_A-Z_0-9_-]+".r
+    val regex="[a-z_A-Z\\d-]+".r
     val it=fromFile(file)
-    try println(s"Number of Words of $file is "+regex.findAllMatchIn(it.mkString).toList.length) finally it.close()
+    try println(s"Number of Words of $file is "+regex.findAllMatchIn(it.mkString).length) finally it.close()
+  }
+  def newCount(file: String):Unit={
+    val it=fromFile(file)
+    try println(s"Number of Words of $file is "+ it.mkString.split("\\s+").length) finally it.close()
   }
   numberWord("file1.txt")
   numberWord("file2.txt")
@@ -20,4 +24,8 @@ object problem2 extends App{
   numberWords("file1.txt")
   numberWords("file2.txt")
   numberWords("file3.txt")
+
+  newCount("file1.txt")
+  newCount("file2.txt")
+  newCount("file3.txt")
 }
